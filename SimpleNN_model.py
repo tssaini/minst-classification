cf98c11ecf5a74d901b733a86fda1186860b3c85
Taranpreet Saini <taranpreet.s16@gmail.com>
2020-03-05 01:58:58 -0500

    switch to tf2, added early stopping and inc size of hidden layers

--- SimpleNN_model.py
+++ SimpleNN_model.py
@@ -1,13 +1,13 @@
-from keras.models import Sequential
-from keras.utils import np_utils
-from keras.layers.core import Dense, Activation, Dropout
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
 
 def simpleNN(input_dim, nb_classes):
     model = Sequential()
-    model.add(Dense(128, input_dim=input_dim))
+    model.add(Dense(200, input_dim=input_dim))
     model.add(Activation('relu'))
     model.add(Dropout(0.15))
-    model.add(Dense(128))
+    model.add(Dense(200))
     model.add(Activation('relu'))
     model.add(Dropout(0.15))
     model.add(Dense(nb_classes))
--- minst_classification.py
+++ minst_classification.py
@@ -1,6 +1,5 @@
-from keras.models import Sequential
-from keras.utils import np_utils
-from keras.layers.core import Dense, Activation, Dropout
+from tensorflow import keras
+from tensorflow.keras import utils
 import SimpleNN_model
 from sklearn.preprocessing import StandardScaler
 
@@ -12,7 +11,7 @@ test = pd.read_csv("test.csv")
 
 train_x = train.iloc[:,1:].values.astype('float32')
 train_y = train.iloc[:,0].values
-train_y = np_utils.to_categorical(train_y) 
+train_y = utils.to_categorical(train_y) 
 
 scaler = StandardScaler()
 train_x = scaler.fit_transform(train_x)
@@ -21,9 +20,9 @@ input_dim = train_x.shape[1]
 nb_classes = train_y.shape[1]
 
 simple_nn = SimpleNN_model.simpleNN(input_dim, nb_classes)
-
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
 print("Training...")
-simple_nn.fit(train_x, train_y, epochs=10, batch_size=10, validation_split=0.1, verbose=2)
+simple_nn.fit(train_x, train_y, epochs=20, batch_size=10, validation_split=0.1, verbose=2, callbacks=[early_stopping_cb])
 
 
 test_x = test.values.astype('float32')
