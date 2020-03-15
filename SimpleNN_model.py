90b39a9bbe5d725bf3a6acfc7a46176d97e750d2
Taranpreet Saini <taranpreet.s16@gmail.com>
2020-03-14 22:33:47 -0400

    98% accuracy

--- SimpleNN_model.py
+++ SimpleNN_model.py
@@ -4,15 +4,22 @@ from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras import optimizers
 
 def simpleNN(input_dim, nb_classes):
-    model = Sequential()
-    model.add(Dense(200, input_dim=input_dim))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.15))
-    model.add(Dense(200))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.15))
-    model.add(Dense(nb_classes))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model = Sequential([
+        keras.layers.Dense(300, activation="relu", input_dim=input_dim),
+        keras.layers.Dense(100, activation="relu"),
+        keras.layers.Dense(nb_classes, activation="softmax")
+    ])
+    model.compile(loss="categorical_crossentropy",
+              optimizer='adam',
+              metrics=["accuracy"])
+    # model = Sequential()
+    # model.add(Dense(300, input_dim=input_dim))
+    # model.add(Activation('relu'))
+    # model.add(Dense(200))
+    # model.add(Activation('relu'))
+    # model.add(Dense(nb_classes))
+    # model.add(Activation('softmax'))
+    # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
--- minst_classification.py
+++ minst_classification.py
@@ -8,24 +8,27 @@ root_logdir = os.path.join(os.curdir, "my_logs")
 
 def get_run_logdir():
     import time
-    run_id = time.strftime("run_%Y_%m_%d-%H_%M_%S")
+    run_id = time.strftime("run_%Y_%m_%d-%H_%M")
     return os.path.join(root_logdir, run_id)
 
 import pandas as pd
 import numpy as np
 
-train = pd.read_csv("train.csv")
+# train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
-train_x = train.iloc[:,1:].values.astype('float32')
-train_y = train.iloc[:,0].values
-train_y = utils.to_categorical(train_y) 
+(X_train_full, y_train_full), (_, _) = keras.datasets.mnist.load_data()
 
-scaler = StandardScaler()
-train_x = scaler.fit_transform(train_x)
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = test / 255.
+y_train = utils.to_categorical(y_train)
+y_valid = utils.to_categorical(y_valid)
 
-input_dim = train_x.shape[1]
-nb_classes = train_y.shape[1]
+X_train = X_train.reshape((X_train.shape[0], -1))
+X_valid = X_valid.reshape((X_valid.shape[0], -1))
+input_dim = X_train.shape[1]
+nb_classes = y_train.shape[1]
 
 simple_nn = SimpleNN_model.simpleNN(input_dim, nb_classes)
 
@@ -33,13 +36,8 @@ run_logdir = get_run_logdir()
 tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
 early_stopping_cb = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
 print("Training...")
-simple_nn.fit(train_x, train_y, epochs=20, batch_size=10, validation_split=0.1, verbose=2, callbacks=[early_stopping_cb, tensorboard_cb])
+simple_nn.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid), callbacks=[early_stopping_cb, tensorboard_cb])
 
-
-test_x = test.values.astype('float32')
-
-test_x = scaler.transform(test_x)
-
-preds = simple_nn.predict_classes(test_x, verbose=0)
+preds = simple_nn.predict_classes(X_test, verbose=0)
 
 pd.DataFrame({"ImageId": list(range(1,len(preds)+1)), "Label": preds}).to_csv("submission.csv", index=False)
